894c2bcb2deb110ea49a573877ef477f8fca3244
Zhenyu Yang <yangzhenyu@sust.edu.cn>
2025-11-07 01:36:10 +0800

    Add initial layout, styles, and main JS file
    Introduces index.css for base styles, updates index.html with navigation, footer, and canvas elements, and adds a placeholder main.js file. Sets up the project structure and includes copyright headers.

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2025 Zhenyu Yang <yangzhenyu@sust.edu.cn>
+ * All rights reserved.
+ *
+ * This source code is the property of Zhenyu Yang.
+ * Unauthorized copying, modification, or distribution of this file,
+ * via any medium, is strictly prohibited.
+ * 
+ * Url: https://github.com/zyang4418/web-eos-project
+ */
